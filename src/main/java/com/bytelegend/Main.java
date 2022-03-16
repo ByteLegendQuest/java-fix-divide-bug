ba714ebcd9326734c5e53db031193e9bca50088b
jaryarbn <grandline501@gmail.com>
2022-03-16 03:12:29 +0000

    Update at 2022-03-16T03:12:29.117160Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -2,7 +2,7 @@ package com.bytelegend;
 
 public class Main {
     public static double divide(int a, int b) {
-        return a / b * 1.0;
+        return 1.0 * a / b;
     }
 
     public static void main(String[] args) {
