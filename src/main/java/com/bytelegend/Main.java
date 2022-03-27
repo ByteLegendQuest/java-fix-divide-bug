9314a919b0e4080aa69744cd3c1f02ce79582e2a
yunghf <yung.hf@outlook.com>
2022-03-27 12:04:58 +0000

    Update at 2022-03-27T12:04:58.424300Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -2,11 +2,11 @@ package com.bytelegend;
 
 public class Main {
     public static double divide(int a, int b) {
-        return (float) a / b;
+        return (double) a / b;
     }
 
     public static void main(String[] args) {
         System.out.println("3/2=" + divide(3, 2));
-        // System.out.println("1/2=" + divide(1, 2));
+        System.out.println("1/2=" + divide(1, 2));
     }
 }
