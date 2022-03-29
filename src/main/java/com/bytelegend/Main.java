42f0d44970bbd075a14d3458d42289be2685916a
Laura-bf <laurabf6@gmail.com>
2022-03-29 20:20:28 +0000

    Update at 2022-03-29T20:20:28.906496Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -2,8 +2,7 @@ package com.bytelegend;
 
 public class Main {
     public static double divide(int a, int b) {
-        double div = a / b;
-        return div;
+        return (double) a / (double) b;
     }
 
     public static void main(String[] args) {
