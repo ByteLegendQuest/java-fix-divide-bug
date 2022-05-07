151dc0ca48f4b2c494d40aca4d62242b64e4a476
kangming06 <kangming06@hotmail.com>
2022-05-07 10:54:07 +0000

    Update at 2022-05-07T10:54:07.671616Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,12 +1,11 @@
 package com.bytelegend;
 
 public class Main {
-    public static double divide(float a, float b) {
-        return a / b;
+    public static double divide(int a, int b) {
+        return (double) a / (double) b;
     }
 
     public static void main(String[] args) {
         System.out.println("3/2=" + divide(3, 2));
-        System.out.println("1/2=" + divide(1, 2));
     }
 }
