bae02335332972d3c5baa8ac34b1662d70a28fda
BestMiltank <topjj0204@163.com>
2022-06-27 06:09:09 +0000

    Update at 2022-06-27T06:09:09.600827Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,11 +1,11 @@
 package com.bytelegend;
 
 public class Main {
-    public static double divide(int a, int b) {
+    public static double divide(double a, double b) {
         return a / b;
     }
 
     public static void main(String[] args) {
-        System.out.println("3/2=" + divide(3, 2));
+        System.out.println("3/2=" + divide(3.0, 2.0));
     }
 }
