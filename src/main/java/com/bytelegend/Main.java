9d382e9dec649766a3df62e6bf5044715c4927e8
hinkleung <394869042@qq.com>
2022-10-14 16:54:08 +0000

    Update at 2022-10-14T16:54:08.886615Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -2,7 +2,7 @@ package com.bytelegend;
 
 public class Main {
     public static double divide(int a, int b) {
-        return a / b * 1d;
+        return a*1.0 / b;
     }
 
     public static void main(String[] args) {
