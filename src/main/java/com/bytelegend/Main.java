e55b2a341cff463743e3f64ee4540229d374bc71
Qiqi7-7 <writetomars@qq.com>
2022-10-28 09:02:25 +0000

    Update at 2022-10-28T09:02:25.024928Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,8 +1,8 @@
 package com.bytelegend;
 
 public class Main {
-    public static double divide(double a, double b) {
-        return (double)a / b;
+    public static double divide(int a, int b) {
+        return ((double)a) / b;
     }
 
     public static void main(String[] args) {
