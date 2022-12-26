c6bc1451eef2f761445b0eed5ee406e0874d98fd
wwwww1wwwww <1302780202@qq.com>
2022-12-26 09:06:18 +0000

    Update at 2022-12-26T09:06:18.186547Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -2,7 +2,7 @@ package com.bytelegend;
 
 public class Main {
     public static double divide(int a, int b) {
-        return a / b;
+        return ((double) a )/ (double) b;
     }
 
     public static void main(String[] args) {
