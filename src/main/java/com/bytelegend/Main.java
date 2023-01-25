d86d6b480f64e73ab0b675183522fd8e79bfe9ec
CITVAIM <citvaim@icloud.com>
2023-01-25 09:44:21 +0000

    Update at 2023-01-25T09:44:21.228047Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -2,15 +2,11 @@ package com.bytelegend;
 
 public class Main {
     public static double divide(int a, int b) {
-         double aa = a;
-         double bb = b;
-         double c = aa / bb;
-         return c;
+         return (double)a / b;
 
     }
 
     public static void main(String[] args) {
         System.out.println("3/2=" + divide(3, 2));
-        System.out.println("1/2=" + divide(1, 2));
     }
 }
